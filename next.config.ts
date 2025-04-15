1143faf363230849dba6959bc0a2086d8cf6863f
2569718930@qq.com <2569718930@qq.com>
2025-04-15 17:37:19 +0800

    更新 Next.js 配置以支持 GitHub Pages 部署，添加 trailingSlash 选项，更新 package.json 和 package-lock.json，设置版本号为 1.0.0。

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true';
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
   output: 'export',
-  // 如果你的仓库名不是根域名，需要设置 basePath 和 assetPrefix
   basePath: isGithubPages ? '/svg2png-tool' : '',
   assetPrefix: isGithubPages ? '/svg2png-tool/' : '',
+  trailingSlash: true, // 避免刷新路径 404
 };
 
 module.exports = nextConfig;
